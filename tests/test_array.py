6fcb2696eafde46fb95df172a2e6a0153d0e36a4
pattonw <pattonw@hhmi.org>
2023-02-15 12:52:57 -0500

    update test to not use daisy

--- tests/test_array.py
+++ tests/test_array.py
@@ -1,4 +1,5 @@
-import daisy
+from funlib.persistence import Array
+from funlib.geometry import Roi, Coordinate
 import numpy as np
 import unittest
 
@@ -8,66 +9,66 @@ class TestArray(unittest.TestCase):
     def test_constructor(self):
 
         data = np.zeros((10, 10, 10), dtype=np.float32)
-        roi = daisy.Roi((0, 0, 0), (10, 10, 10))
+        roi = Roi((0, 0, 0), (10, 10, 10))
 
         # consistent configurations
-        daisy.Array(data, roi, (1, 1, 1))
-        daisy.Array(data, roi, (1, 1, 2))
-        daisy.Array(data, roi, (1, 5, 2))
-        daisy.Array(data, roi, (10, 5, 2))
-        roi = daisy.Roi((1, 1, 1), (10, 10, 10))
-        daisy.Array(data, roi, (1, 1, 1), data_offset=(1, 1, 1))
-        roi = daisy.Roi((0, 0, 0), (20, 20, 20))
-        daisy.Array(data, roi, (2, 2, 2))
+        Array(data, roi, (1, 1, 1))
+        Array(data, roi, (1, 1, 2))
+        Array(data, roi, (1, 5, 2))
+        Array(data, roi, (10, 5, 2))
+        roi = Roi((1, 1, 1), (10, 10, 10))
+        Array(data, roi, (1, 1, 1), data_offset=(1, 1, 1))
+        roi = Roi((0, 0, 0), (20, 20, 20))
+        Array(data, roi, (2, 2, 2))
 
         # dims don't match
         with self.assertRaises(AssertionError):
-            daisy.Array(data, roi, (1, 1))
+            Array(data, roi, (1, 1))
 
         # ROI not multiple of voxel size
         with self.assertRaises(AssertionError):
-            daisy.Array(data, roi, (1, 1, 3))
+            Array(data, roi, (1, 1, 3))
         with self.assertRaises(AssertionError):
-            daisy.Array(data, roi, (1, 1, 4))
+            Array(data, roi, (1, 1, 4))
 
         # ROI begin doesn't align with voxel size
-        roi = daisy.Roi((1, 1, 1), (11, 11, 11))
+        roi = Roi((1, 1, 1), (11, 11, 11))
         with self.assertRaises(AssertionError):
-            daisy.Array(data, roi, (1, 1, 2))
+            Array(data, roi, (1, 1, 2))
 
         # ROI shape doesn't align with voxel size
-        roi = daisy.Roi((0, 0, 0), (11, 11, 11))
+        roi = Roi((0, 0, 0), (11, 11, 11))
         with self.assertRaises(AssertionError):
-            daisy.Array(data, roi, (1, 1, 2))
+            Array(data, roi, (1, 1, 2))
 
         # ROI outside of provided data
-        roi = daisy.Roi((0, 0, 0), (20, 20, 20))
+        roi = Roi((0, 0, 0), (20, 20, 20))
         with self.assertRaises(AssertionError):
-            daisy.Array(data, roi, (1, 1, 1))
+            Array(data, roi, (1, 1, 1))
         with self.assertRaises(AssertionError):
-            daisy.Array(data, roi, (2, 2, 1))
+            Array(data, roi, (2, 2, 1))
         with self.assertRaises(AssertionError):
-            daisy.Array(data, roi, (2, 2, 2), data_offset=(0, 0, 2))
+            Array(data, roi, (2, 2, 2), data_offset=(0, 0, 2))
 
     def test_shape(self):
 
         # ROI fits data
 
-        a1 = daisy.Array(
+        a1 = Array(
             np.zeros((10,)),
-            daisy.Roi((0,), (10,)),
+            Roi((0,), (10,)),
             (1,))
-        a2 = daisy.Array(
+        a2 = Array(
             np.zeros((10, 10)),
-            daisy.Roi((0, 0), (10, 10)),
+            Roi((0, 0), (10, 10)),
             (1, 1))
-        a2_3 = daisy.Array(
+        a2_3 = Array(
             np.zeros((3, 10, 10)),
-            daisy.Roi((0, 0), (10, 10)),
+            Roi((0, 0), (10, 10)),
             (1, 1))
-        a5_3_2_1 = daisy.Array(
+        a5_3_2_1 = Array(
             np.zeros((1, 2, 3, 4, 4, 4, 4, 4)),
-            daisy.Roi((0, 0, 0, 0, 0), (80, 80, 80, 80, 80)),
+            Roi((0, 0, 0, 0, 0), (80, 80, 80, 80, 80)),
             (20, 20, 20, 20, 20))
 
         assert a1.shape == (10,)
@@ -78,21 +79,21 @@ class TestArray(unittest.TestCase):
 
         # ROI subset of data
 
-        a1 = daisy.Array(
+        a1 = Array(
             np.zeros((20,)),
-            daisy.Roi((0,), (10,)),
+            Roi((0,), (10,)),
             (1,))
-        a2 = daisy.Array(
+        a2 = Array(
             np.zeros((20, 20)),
-            daisy.Roi((0, 0), (10, 10)),
+            Roi((0, 0), (10, 10)),
             (1, 1))
-        a2_3 = daisy.Array(
+        a2_3 = Array(
             np.zeros((3, 20, 20)),
-            daisy.Roi((0, 0), (10, 10)),
+            Roi((0, 0), (10, 10)),
             (1, 1))
-        a5_3_2_1 = daisy.Array(
+        a5_3_2_1 = Array(
             np.zeros((1, 2, 3, 5, 5, 5, 5, 5)),
-            daisy.Roi((0, 0, 0, 0, 0), (80, 80, 80, 80, 80)),
+            Roi((0, 0, 0, 0, 0), (80, 80, 80, 80, 80)),
             (20, 20, 20, 20, 20))
 
         assert a1.shape == (10,)
@@ -104,167 +105,167 @@ class TestArray(unittest.TestCase):
     def test_dtype(self):
 
         for dtype in [np.float32, np.uint8, np.uint64]:
-            assert daisy.Array(
+            assert Array(
                 np.zeros((1,), dtype=dtype),
-                daisy.Roi((0,), (1,)),
+                Roi((0,), (1,)),
                 (1,)).dtype == dtype
 
     def test_getitem(self):
 
-        a = daisy.Array(
+        a = Array(
             np.arange(0, 10).reshape(2, 5),
-            daisy.Roi((0, 0), (2, 5)),
+            Roi((0, 0), (2, 5)),
             (1, 1))
 
-        assert a[daisy.Coordinate((0, 0))] == 0
-        assert a[daisy.Coordinate((0, 1))] == 1
-        assert a[daisy.Coordinate((0, 2))] == 2
-        assert a[daisy.Coordinate((1, 0))] == 5
-        assert a[daisy.Coordinate((1, 1))] == 6
+        assert a[Coordinate((0, 0))] == 0
+        assert a[Coordinate((0, 1))] == 1
+        assert a[Coordinate((0, 2))] == 2
+        assert a[Coordinate((1, 0))] == 5
+        assert a[Coordinate((1, 1))] == 6
         with self.assertRaises(AssertionError):
-            a[daisy.Coordinate((1, 5))]
+            a[Coordinate((1, 5))]
         with self.assertRaises(AssertionError):
-            a[daisy.Coordinate((2, 5))]
+            a[Coordinate((2, 5))]
         with self.assertRaises(AssertionError):
-            a[daisy.Coordinate((-1, 0))]
+            a[Coordinate((-1, 0))]
         with self.assertRaises(AssertionError):
-            a[daisy.Coordinate((0, -1))]
+            a[Coordinate((0, -1))]
 
-        b = a[daisy.Roi((1, 1), (1, 4))]
+        b = a[Roi((1, 1), (1, 4))]
         with self.assertRaises(AssertionError):
-            b[daisy.Coordinate((0, 0))] == 0
+            b[Coordinate((0, 0))] == 0
         with self.assertRaises(AssertionError):
-            b[daisy.Coordinate((0, 1))] == 1
+            b[Coordinate((0, 1))] == 1
         with self.assertRaises(AssertionError):
-            b[daisy.Coordinate((0, 2))] == 2
+            b[Coordinate((0, 2))] == 2
         with self.assertRaises(AssertionError):
-            b[daisy.Coordinate((1, 0))] == 5
-        assert b[daisy.Coordinate((1, 1))] == 6
-        assert b[daisy.Coordinate((1, 2))] == 7
-        assert b[daisy.Coordinate((1, 3))] == 8
-        assert b[daisy.Coordinate((1, 4))] == 9
+            b[Coordinate((1, 0))] == 5
+        assert b[Coordinate((1, 1))] == 6
+        assert b[Coordinate((1, 2))] == 7
+        assert b[Coordinate((1, 3))] == 8
+        assert b[Coordinate((1, 4))] == 9
         with self.assertRaises(AssertionError):
-            b[daisy.Coordinate((1, 5))]
+            b[Coordinate((1, 5))]
         with self.assertRaises(AssertionError):
-            b[daisy.Coordinate((2, 5))]
+            b[Coordinate((2, 5))]
         with self.assertRaises(AssertionError):
-            b[daisy.Coordinate((-1, 0))]
+            b[Coordinate((-1, 0))]
         with self.assertRaises(AssertionError):
-            b[daisy.Coordinate((0, -1))]
+            b[Coordinate((0, -1))]
 
     def test_setitem(self):
 
         # set entirely with numpy array
 
-        a = daisy.Array(
+        a = Array(
             np.zeros((2, 5)),
-            daisy.Roi((0, 0), (2, 5)),
+            Roi((0, 0), (2, 5)),
             (1, 1))
 
-        a[daisy.Roi((0, 0), (2, 5))] = np.arange(0, 10).reshape(2, 5)
-        assert a[daisy.Coordinate((0, 0))] == 0
-        assert a[daisy.Coordinate((0, 1))] == 1
-        assert a[daisy.Coordinate((0, 2))] == 2
-        assert a[daisy.Coordinate((1, 0))] == 5
-        assert a[daisy.Coordinate((1, 1))] == 6
-        assert a[daisy.Coordinate((1, 4))] == 9
+        a[Roi((0, 0), (2, 5))] = np.arange(0, 10).reshape(2, 5)
+        assert a[Coordinate((0, 0))] == 0
+        assert a[Coordinate((0, 1))] == 1
+        assert a[Coordinate((0, 2))] == 2
+        assert a[Coordinate((1, 0))] == 5
+        assert a[Coordinate((1, 1))] == 6
+        assert a[Coordinate((1, 4))] == 9
 
         # set entirely with numpy array and channels
 
-        a = daisy.Array(
+        a = Array(
             np.zeros((3, 2, 5)),
-            daisy.Roi((0, 0), (2, 5)),
+            Roi((0, 0), (2, 5)),
             (1, 1))
 
-        a[daisy.Roi((0, 0), (2, 5))] = np.arange(0, 3*10).reshape(3, 2, 5)
-        np.testing.assert_array_equal(a[daisy.Coordinate((0, 0))], [0, 10, 20])
-        np.testing.assert_array_equal(a[daisy.Coordinate((0, 1))], [1, 11, 21])
-        np.testing.assert_array_equal(a[daisy.Coordinate((1, 4))], [9, 19, 29])
+        a[Roi((0, 0), (2, 5))] = np.arange(0, 3*10).reshape(3, 2, 5)
+        np.testing.assert_array_equal(a[Coordinate((0, 0))], [0, 10, 20])
+        np.testing.assert_array_equal(a[Coordinate((0, 1))], [1, 11, 21])
+        np.testing.assert_array_equal(a[Coordinate((1, 4))], [9, 19, 29])
 
         # set entirely with scalar
 
-        a = daisy.Array(
+        a = Array(
             np.zeros((2, 5)),
-            daisy.Roi((0, 0), (2, 5)),
+            Roi((0, 0), (2, 5)),
             (1, 1))
 
-        a[daisy.Roi((0, 0), (2, 5))] = 42
-        assert a[daisy.Coordinate((0, 0))] == 42
-        assert a[daisy.Coordinate((1, 4))] == 42
+        a[Roi((0, 0), (2, 5))] = 42
+        assert a[Coordinate((0, 0))] == 42
+        assert a[Coordinate((1, 4))] == 42
 
         # set partially with scalar and channels
 
-        a = daisy.Array(
+        a = Array(
             np.arange(0, 3*10).reshape(3, 2, 5),
-            daisy.Roi((0, 0), (2, 5)),
+            Roi((0, 0), (2, 5)),
             (1, 1))
 
-        a[daisy.Roi((0, 0), (2, 2))] = 42
+        a[Roi((0, 0), (2, 2))] = 42
         np.testing.assert_array_equal(
-            a[daisy.Coordinate((0, 0))], [42, 42, 42])
+            a[Coordinate((0, 0))], [42, 42, 42])
         np.testing.assert_array_equal(
-            a[daisy.Coordinate((0, 1))], [42, 42, 42])
+            a[Coordinate((0, 1))], [42, 42, 42])
         np.testing.assert_array_equal(
-            a[daisy.Coordinate((0, 2))], [2, 12, 22])
+            a[Coordinate((0, 2))], [2, 12, 22])
         np.testing.assert_array_equal(
-            a[daisy.Coordinate((1, 2))], [7, 17, 27])
+            a[Coordinate((1, 2))], [7, 17, 27])
         np.testing.assert_array_equal(
-            a[daisy.Coordinate((1, 3))], [8, 18, 28])
+            a[Coordinate((1, 3))], [8, 18, 28])
         np.testing.assert_array_equal(
-            a[daisy.Coordinate((1, 4))], [9, 19, 29])
+            a[Coordinate((1, 4))], [9, 19, 29])
 
         # set partially with Array
 
-        a = daisy.Array(
+        a = Array(
             np.zeros((2, 5)),
-            daisy.Roi((0, 0), (2, 5)),
+            Roi((0, 0), (2, 5)),
             (1, 1))
-        b = daisy.Array(
+        b = Array(
             np.arange(0, 10).reshape(2, 5),
-            daisy.Roi((0, 0), (2, 5)),
+            Roi((0, 0), (2, 5)),
             (1, 1))
 
-        a[daisy.Roi((0, 0), (1, 5))] = b[daisy.Roi((0, 0), (1, 5))]
-        assert a[daisy.Coordinate((0, 0))] == 0
-        assert a[daisy.Coordinate((0, 1))] == 1
-        assert a[daisy.Coordinate((0, 2))] == 2
-        assert a[daisy.Coordinate((1, 0))] == 0
-        assert a[daisy.Coordinate((1, 1))] == 0
-        assert a[daisy.Coordinate((1, 4))] == 0
+        a[Roi((0, 0), (1, 5))] = b[Roi((0, 0), (1, 5))]
+        assert a[Coordinate((0, 0))] == 0
+        assert a[Coordinate((0, 1))] == 1
+        assert a[Coordinate((0, 2))] == 2
+        assert a[Coordinate((1, 0))] == 0
+        assert a[Coordinate((1, 1))] == 0
+        assert a[Coordinate((1, 4))] == 0
 
-        a = daisy.Array(
+        a = Array(
             np.zeros((2, 5)),
-            daisy.Roi((0, 0), (2, 5)),
+            Roi((0, 0), (2, 5)),
             (1, 1))
-        b = daisy.Array(
+        b = Array(
             np.arange(0, 10).reshape(2, 5),
-            daisy.Roi((0, 0), (2, 5)),
+            Roi((0, 0), (2, 5)),
             (1, 1))
 
-        a[daisy.Roi((0, 0), (1, 5))] = b[daisy.Roi((1, 0), (1, 5))]
-        assert a[daisy.Coordinate((0, 0))] == 5
-        assert a[daisy.Coordinate((0, 1))] == 6
-        assert a[daisy.Coordinate((0, 4))] == 9
-        assert a[daisy.Coordinate((1, 0))] == 0
-        assert a[daisy.Coordinate((1, 1))] == 0
-        assert a[daisy.Coordinate((1, 2))] == 0
+        a[Roi((0, 0), (1, 5))] = b[Roi((1, 0), (1, 5))]
+        assert a[Coordinate((0, 0))] == 5
+        assert a[Coordinate((0, 1))] == 6
+        assert a[Coordinate((0, 4))] == 9
+        assert a[Coordinate((1, 0))] == 0
+        assert a[Coordinate((1, 1))] == 0
+        assert a[Coordinate((1, 2))] == 0
 
-        a[daisy.Roi((1, 0), (1, 5))] = b[daisy.Roi((0, 0), (1, 5))]
-        assert a[daisy.Coordinate((0, 0))] == 5
-        assert a[daisy.Coordinate((0, 1))] == 6
-        assert a[daisy.Coordinate((0, 4))] == 9
-        assert a[daisy.Coordinate((1, 0))] == 0
-        assert a[daisy.Coordinate((1, 1))] == 1
-        assert a[daisy.Coordinate((1, 2))] == 2
+        a[Roi((1, 0), (1, 5))] = b[Roi((0, 0), (1, 5))]
+        assert a[Coordinate((0, 0))] == 5
+        assert a[Coordinate((0, 1))] == 6
+        assert a[Coordinate((0, 4))] == 9
+        assert a[Coordinate((1, 0))] == 0
+        assert a[Coordinate((1, 1))] == 1
+        assert a[Coordinate((1, 2))] == 2
 
     def test_materialize(self):
 
-        a = daisy.Array(
+        a = Array(
             np.arange(0, 10).reshape(2, 5),
-            daisy.Roi((0, 0), (2, 5)),
+            Roi((0, 0), (2, 5)),
             (1, 1))
 
-        b = a[daisy.Roi((0, 0), (2, 2))]
+        b = a[Roi((0, 0), (2, 2))]
 
         # underlying data did not change
         assert a.data.shape == b.data.shape
@@ -277,27 +278,27 @@ class TestArray(unittest.TestCase):
 
     def test_to_ndarray(self):
 
-        a = daisy.Array(
+        a = Array(
             np.arange(0, 10).reshape(2, 5),
-            daisy.Roi((0, 0), (2, 5)),
+            Roi((0, 0), (2, 5)),
             (1, 1))
 
         # not within ROI of a and no fill value provided
         with self.assertRaises(AssertionError):
-            a.to_ndarray(daisy.Roi((0, 0), (5, 5)))
+            a.to_ndarray(Roi((0, 0), (5, 5)))
 
-        b = a.to_ndarray(daisy.Roi((0, 0), (1, 5)))
+        b = a.to_ndarray(Roi((0, 0), (1, 5)))
         compare = np.array([[0, 1, 2, 3, 4]])
 
-        b = a.to_ndarray(daisy.Roi((1, 0), (1, 5)))
+        b = a.to_ndarray(Roi((1, 0), (1, 5)))
         compare = np.array([[5, 6, 7, 8, 9]])
 
-        b = a.to_ndarray(daisy.Roi((0, 0), (2, 2)))
+        b = a.to_ndarray(Roi((0, 0), (2, 2)))
         compare = np.array([[0, 1], [5, 6]])
 
         np.testing.assert_array_equal(b, compare)
 
-        b = a.to_ndarray(daisy.Roi((0, 0), (5, 5)), fill_value=1)
+        b = a.to_ndarray(Roi((0, 0), (5, 5)), fill_value=1)
         compare = np.array([
             [0, 1, 2, 3, 4],
             [5, 6, 7, 8, 9],
@@ -309,12 +310,12 @@ class TestArray(unittest.TestCase):
 
     def test_intersect(self):
 
-        a = daisy.Array(
+        a = Array(
             np.arange(0, 10).reshape(2, 5),
-            daisy.Roi((0, 0), (2, 5)),
+            Roi((0, 0), (2, 5)),
             (1, 1))
 
-        b = a.intersect(daisy.Roi((1, 1), (10, 10)))
+        b = a.intersect(Roi((1, 1), (10, 10)))
 
-        assert b.roi == daisy.Roi((1, 1), (1, 4))
+        assert b.roi == Roi((1, 1), (1, 4))
         np.testing.assert_array_equal(b.to_ndarray(), [[6, 7, 8, 9]])
