dcaa5413146fb669aea2da680c90fe1ffbb7bfbf
William Patton <wllmpttn24@gmail.com>
2023-11-15 18:44:01 -0700

    remove old graph providers

--- funlib/persistence/graphs/__init__.py
+++ funlib/persistence/graphs/__init__.py
@@ -1,3 +1,1 @@
-from .file_graph_provider import FileGraphProvider  # noqa
-from .mongodb_graph_provider import MongoDbGraphProvider  # noqa
-from .sqlite_graph_provider import SQLiteGraphProvider  # noqa
+from .sqlite_graph_database import SQLiteGraphDataBase  # noqa
