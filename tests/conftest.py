0b6de721fb5ced7d3b76129fdcb097150976f628
William Patton <wllmpttn24@gmail.com>
2024-07-09 12:58:10 -0700

    only run psql tests if psql is available

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,9 +4,38 @@ import pytest
 import pymongo
 
 from pathlib import Path
+import psycopg2
+from psycopg2 import OperationalError
 
 
-@pytest.fixture(params=(pytest.param("sqlite"), pytest.param("psql")))
+# Attempt to connect to the default database
+def can_connect_to_psql():
+    try:
+        conn = psycopg2.connect(
+            dbname="pytest",
+        )
+        conn.close()
+        return True
+    except OperationalError:
+        return False
+
+
+# Conditionally mark the "psql" parameter
+psql_param = (
+    pytest.param(
+        "psql", marks=pytest.mark.skip(reason="Cannot connect to psql database")
+    )
+    if not can_connect_to_psql()
+    else "psql"
+)
+
+
+@pytest.fixture(
+    params=(
+        pytest.param("sqlite"),
+        psql_param,
+    )
+)
 def provider_factory(request, tmpdir):
     # provides a factory function to generate graph provider
     # can provide either mongodb graph provider or file graph provider
