777b9aa9180458a5d2ce3c12a3641c3f391dfae8
William Patton <wllmpttn24@gmail.com>
2024-12-13 13:43:20 -0800

    Add support for custom metadata. Tested :)

--- funlib/persistence/arrays/array.py
+++ funlib/persistence/arrays/array.py
@@ -5,6 +5,7 @@ from typing import Optional, Sequence, Union
 import dask.array as da
 import numpy as np
 from dask.array.optimization import fuse_slice
+from zarr import Array as ZarrArray
 
 from funlib.geometry import Coordinate, Roi
 
@@ -83,6 +84,10 @@ class Array(Freezable):
             shape=self._source_data.shape,
         )
 
+        # used for custom metadata unrelated to indexing with physical units
+        # only used if not reading from zarr and there is no built in `.attrs`
+        self._attrs = {}
+
         if lazy_op is not None:
             self.apply_lazy_ops(lazy_op)
 
@@ -92,6 +97,18 @@ class Array(Freezable):
         self.freeze()
 
         self.validate()
+
+    @property
+    def attrs(self) -> dict:
+        """
+        Return dict that can be used to store custom metadata. Will be persistent
+        for zarr arrays. If reading from zarr, any existing metadata (such as
+        voxel_size, axis_names, etc.) will also be exposed here.
+        """
+        if isinstance(self._source_data, ZarrArray):
+            return self._source_data.attrs
+        else:
+            return self._attrs
 
     @property
     def chunk_shape(self) -> Coordinate:
