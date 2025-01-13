b181f92ce67879b5440f8c2548df8637b7400cdf
William Patton <wllmpttn24@gmail.com>
2025-01-13 10:14:52 -0800

    patch 0.5.4 bump

--- funlib/persistence/__init__.py
+++ funlib/persistence/__init__.py
@@ -1,4 +1,4 @@
 from .arrays import Array, open_ds, prepare_ds, open_ome_ds, prepare_ome_ds  # noqa
 
-__version__ = "0.5.3"
+__version__ = "0.5.4"
 __version_info__ = tuple(int(i) for i in __version__.split("."))
